12699b9baa8a65287b8b9823bab374cc7b63734e
Guram Khasia <gurikakhasia@gmail.com>
2020-01-13 12:20:27 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, createRef } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    validateLanguages();
+  }, []);
+
+  const baseLanguageOptions = createRef();
+  const targetLanguageOptions = createRef();
+  const validateLanguages = () => {
+    console.log(
+      "--------------- baseLanguageOptions.current.options ---------"
+    );
+    console.log(baseLanguageOptions.current.options);
+
+    console.log("--------------- targetLanguageOptions ---------");
+    console.log(targetLanguageOptions);
+
+    console.log(
+      "--------------- targetLanguageOptions.current.value ---------"
+    );
+    console.log(targetLanguageOptions.current.value);
+    console.log("---------------  ---------");
+
+    for (let i = 0; i < baseLanguageOptions.current.options.length; i++) {
+      console.log(
+        "baseLanguageOptions.current.options[i].value",
+        baseLanguageOptions.current.options[i].value
+      );
+      if (
+        baseLanguageOptions.current.options[i].value ===
+        targetLanguageOptions.current.value
+      ) {
+        baseLanguageOptions.current.options[i].disabled = "true";
+        console.log("if");
+      } else {
+        baseLanguageOptions.current.options[i].removeAttribute("disabled");
+      }
+    }
+
+    for (let i = 0; i < targetLanguageOptions.current.options.length; i++) {
+      console.log(
+        "targetLanguageOptions.current.options[i].value",
+        targetLanguageOptions.current.options[i].value
+      );
+      if (
+        targetLanguageOptions.current.options[i].value ===
+        baseLanguageOptions.current.value
+      ) {
+        targetLanguageOptions.current.options[i].disabled = "true";
+      } else {
+        targetLanguageOptions.current.options[i].removeAttribute("disabled");
+      }
+    }
+    console.log("test");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        ref={baseLanguageOptions}
+        name="baseSelect"
+        onChange={validateLanguages}
+      >
+        <option value="en">en</option>
+        <option value="ge">ge</option>
+        <option value="ru">ru</option>
+      </select>
+      <select
+        ref={targetLanguageOptions}
+        name="targetSelect"
+        onChange={validateLanguages}
+      >
+        <option value="ge">ge</option>
+        <option value="en">en</option>
+        <option value="ru">ru</option>
+      </select>
     </div>
   );
 }
